d208a1a6504c92eba5278eae8a47ce605846dde9
mahilumphil <phil.yupte@gmail.com>
2017-11-11 11:07:06 +0800

    Sample Push

---
+++ POSApi/Entities/MstCustomer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace POSApi.Entities
+{
+    public class MstCustomer
+    {
+        public Int32 Id { get; set; }
+        public String Customer { get; set; }
+        public String Address { get; set; }
+        public String ContactPerson { get; set; }
+        public String ContactNumber { get; set; }
+        public Decimal CreditLimit { get; set; }
+        public Int32 TermId { get; set; }
+        public String TIN { get; set; }
+        public Boolean WithReward { get; set; }
+    }
+}
