27e71ae3c334321692f59401c8f7268eb39ab3eb
Noah Oliver Rigonan <oliverrigonan@gmail.com>
2018-02-14 14:39:09 +0800

    Added New File (Kitchen Report Template)

---
+++ POSApi/ApiControllers/ApiSysPrintKitchenReportController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.Globalization;
+using System.Linq;
+using System.Web.Http;
+
+namespace POSApi.ApiControllers
+{
+    [RoutePrefix("api/print/kitchen1Report")]
+    public class ApiSysPrintKitchenReportController : ApiController
+    {
+        // ============
+        // Data Context
+        // ============
+        public Data.posDBDataContext db = new Data.posDBDataContext();
+
+        // ================
+        // Global Variables
+        // ================
+        private Int32 salesLineId = 0;
+
+        // ==========
+        // Print Page
+        // ==========
+        [HttpGet, Route("ketchen1/{id}")]
+        public void Printer(String id)
+        {
+            try
+            {
+                salesLineId = Convert.ToInt32(id);
+
+                PrinterSettings ps = new PrinterSettings
+                {
+                    PrinterName = "EPSON TM-T81 Receipt"
+                };
+
+                PrintDocument pd = new PrintDocument();
+                pd.PrintPage += new PrintPageEventHandler(PrintPage);
+                pd.PrinterSettings = ps;
+                pd.Print();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+        }
+
+        // ==========
+        // Print Page
+        // ==========
+        public void PrintPage(object sender, PrintPageEventArgs ev)
+        {
+            // =============
+            // Font Settings
+            // =============
+            Font fontArial12Bold = new Font("Arial", 12, FontStyle.Bold);
+            Font fontArial12Regular = new Font("Arial", 12, FontStyle.Regular);
+            Font fontArial11Bold = new Font("Arial", 11, FontStyle.Bold);
+            Font fontArial11Regular = new Font("Arial", 11, FontStyle.Regular);
+            Font fontArial8Bold = new Font("Arial", 8, FontStyle.Bold);
+            Font fontArial8Regular = new Font("Arial", 8, FontStyle.Regular);
+
+            // ==================
+            // Alignment Settings
+            // ==================
+            StringFormat drawFormatCenter = new StringFormat { Alignment = StringAlignment.Center };
+            StringFormat drawFormatLeft = new StringFormat { Alignment = StringAlignment.Near };
+            StringFormat drawFormatRight = new StringFormat { Alignment = StringAlignment.Far };
+
+            float x = 5, y = 5;
+            float width = 270.0F, height = 0F;
+
+            // ==============
+            // Tools Settings
+            // ==============
+            SolidBrush drawBrush = new SolidBrush(Color.Black);
+            Pen blackPen = new Pen(Color.Black, 1);
+            Graphics graphics = ev.Graphics;
+        }
+    }
+}
